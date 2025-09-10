5ea9f0c061b36a25351860027968db02ccd3a44e
lucas-devworks <lucasteixeirasantos74@gmail.com>
2025-09-09 21:36:35 -0300

    atualização

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-let url_projeto = "https://desafio-15-dias-ab748-default-rtdb.firebaseio.com/";
+let url_projeto = "https://desafio-15-dias-ab748-default-rtdb.firebaseio.com";
 
 let estaEditando =  false;
 
-// LISTA AS TAREFAS ARMAZENADAS NO BANCO
+// LISTA AS TAREFAS ARMAZENADAS NO BANCO (R)
 function getLista() {
-    let html = '';
+    let html = "";
     let listaDeTarefaSalvas = document.getElementById("listaDeTarefaSalvas");
 
-    fetch(url_projeto + "tarefas.json").then((response => {
+    fetch(url_projeto + "/tarefas.json").then((response => {
         
         if(response.status === 200) {
             response.json().then(dados => {
                 let arrayListaTarefas = Object.entries(dados);
                     arrayListaTarefas.forEach(elemento => {
-                        html += montarLista(elemento[1]);
+                        html += montarLista(elemento[1], elemento[0]);
                     });
-                    listaDeTarefaSalvas.innerHTML = html;
+                        listaDeTarefaSalvas.innerHTML = html;
             })
         }
-    }));
-};
 
-function montarLista (tarefa) {
+    }))
 
-     return `<li id="'${tarefa.id}'"> 
+    };
+
+
+function montarLista (tarefa, idBanco) {
+
+     return `<li class="tarefa" id="'${tarefa.id}'"> 
                 ${tarefa.titulo}
                 ${tarefa.descricao}
-            <button onclick="editarTarefa('${tarefa.id}')">Editar</button>
-            <button onclick="deletarTarefa('${tarefa.id}')">Deletar</button>
+            <button class="btn_edi" onclick="editarTarefa('${tarefa.id}', '${idBanco}')">Editar</button>
+            <button class="btn_del" onclick="deletarTarefa('${idBanco}')">Deletar</button>
         </li>`;
 };
-// edita uma tarefa
-function editarTarefa(id) {
+
+// EDITAR TAREFA (U)
+function editarTarefa(id, idBanco) {
     if(!estaEditando) {
     let liParaEditar = document.getElementById(`'${id}'`);
         const html = `<div>
@@ -43,58 +47,104 @@ function editarTarefa(id) {
                                 <label>Editar Descrição</label><br>
                                 <textarea id="descricao" placeholder="descrição da terafa"></textarea>    
                             </div>
-                             <button onclick="salvarTarefa()">Editar</button>
+                             <button onclick="salvarTarefa('${idBanco}')">Confirmar</button>
+                             <button onclick="cancelarEdit()">Cancelar</button>
                         </div>`;
         liParaEditar.innerHTML = html;
         estaEditando = true;
     };
 };
 
-function salvarTarefa () {
-    alert("teste")
-};
+function salvarTarefa (idBanco) { 
+    const titulo = document.getElementById('titulo').value;
+    const descricao = document.getElementById('descricao').value;
 
-
-
-function deletarTarefa(id) {
-    alert('d');
-}
-
-
-// FAZ UM REGISTRO NO BANCO DE DADOS
-function criarTarefa () {
-
-    let titulo = document.getElementById("titulo").value;
-    let descricao = document.getElementById("descricao").value;
-    let resposta = document.getElementById("mensagem");
-
-    // OBJETO
+      // OBJETO
     const tarefa = {
         id: new Date().toISOString(),
         titulo: titulo,
         descricao: descricao,
     };
 
-    // ENVIO PARA O FIREBASE
-    try {
-        fetch(url_projeto + "tarefas.json", {
-            method: 'POST',
+    // ENVIO ALTERAÇÃO PARA FIREBASE
+        fetch(url_projeto + `/tarefas/${idBanco}.json`, {
+            method: 'PATCH',
             headers: {'Content-Type': 'application/json'
             },
             body: JSON.stringify(tarefa),
-        }).then( (response) => {
-            console.log(response.status);
+        }).then(response => {
+            if(response.status === 200) {
+                estaEditando = false;
+                getLista();
             
-            if (response.status === 200) {
-                resposta.innerHTML = `<b>Salvo com Sucesso</b>`;
-            } else {
-                resposta.innerHTML = `<b>Erro ao Salvar</b>`;
-
             }
-        } );
-    } catch (error) {
-        console.log(error);
-        resposta.innerHTML = `<i>${error}</i>`;
-    }
+        });
+};
+
+
+function deletarTarefa(idBanco) {
+
+    const confirme = confirm("Tem certeza que deseja deletar esta tarefa?");
+    if(confirme) {
+
+        fetch(url_projeto + `/tarefas/${idBanco}.json`, {
+         method: 'DELETE',
+        }).then(response => {
+            if(response.status === 200) {
+                getLista();
+            };
+        });
+    };
 
 };
+
+
+// FAZ UM REGISTRO NO BANCO DE DADOS (C)
+function criarTarefa () {
+
+    let titulo = document.getElementById("titulo").value;
+    let descricao = document.getElementById("descricao").value;
+    let resposta = document.getElementById("mensagem");
+
+    if(titulo.length <= 0 || descricao.length <= 0) { // verifica se os inputs contem informações
+        resposta.innerHTML = `<b>input vazio</b>`;
+    } else {
+
+        // OBJETO
+        const tarefa = {
+            id: new Date().toISOString(),
+            titulo: titulo,
+            descricao: descricao,
+        };
+
+        // ENVIO PARA O FIREBASE
+        try {
+            fetch(url_projeto + "/tarefas.json", {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(tarefa),
+            }).then( (response) => {
+                console.log(response.status);
+                
+                if (response.status === 200) {
+                    resposta.innerHTML = `<b>Salvo com Sucesso</b>`;
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 2000);// recarrega tela após registro da terafa no banco
+                } else {
+                    resposta.innerHTML = `<b>Erro ao Salvar</b>`;
+
+                }
+            } );
+        } catch (error) {
+            console.log(error);
+            resposta.innerHTML = `<i>${error}</i>`;
+        }
+    };
+};
+
+// recarrega a tela caso o usuário não queira editar a tarefa mais
+function cancelarEdit() {
+        window.location.reload();
+}
